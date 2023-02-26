c991359b8acc682f24f1de8a95f6a6f31df437a1
himmat404 <himmatramchaudhary@gmail.com>
2023-02-26 16:22:57 +0530

    Calculates

---
+++ OOP with Java/Practical-5/Calculates.java
@@ -0,0 +1,16 @@
+public class Calculates {
+    public static void main(String[] args) {
+        
+        int num=Integer.parseInt(args[0]);  
+
+        if(num<=50000){
+            System.out.println("Income tax is Nil");
+        }else if (num>50000 && num<=60000){
+            System.out.println("Income tax is "+(num*0.1));
+        }else if (num>60000 && num<=150000){
+            System.out.println("Income tax is "+(num*0.2));
+        }else if (num>150000){
+            System.out.println("Income tax is "+(num*0.3));
+        }
+    }
+}
