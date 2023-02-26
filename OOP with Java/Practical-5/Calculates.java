acadb2abd8ca45f0e3da819d740b7963fed4aa38
himmat404 <himmatramchaudhary@gmail.com>
2023-02-26 19:23:23 +0530

    Final

--- OOP with Java/Practical-5/Calculates.java
+++ OOP with Java/Practical-5/Calculates.java
@@ -4,7 +4,7 @@ public class Calculates {
         int num=Integer.parseInt(args[0]);  
 
         if(num<=50000){
-            System.out.println("Income tax is Nil");
+            System.out.println("Income tax is 0");
         }else if (num>50000 && num<=60000){
             System.out.println("Income tax is "+(num*0.1));
         }else if (num>60000 && num<=150000){
--- OOP with Java/Practical-5/PalindromeString.java
+++ OOP with Java/Practical-5/PalindromeString.java
@@ -10,14 +10,12 @@ public class PalindromeString {
             System.out.println("The word "+str+" is Palindrome. ");
         else
             System.out.println("The word "+str+" is not a Palindrome. ");
-
     }
     static boolean palindrome(String word){
 
         int firstIndex = 0;
         int lastIndex = word.length() - 1;
         while(firstIndex < lastIndex){
-
             if(word.charAt(firstIndex)!=word.charAt(lastIndex)){
                 return false;
             }
