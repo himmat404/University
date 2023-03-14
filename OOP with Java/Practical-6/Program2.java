d562700d664271573bc8491f6e95ac96f9d5c87a
himmat404 <himmatramchaudhary@gmail.com>
2023-03-14 15:06:14 +0530

    Update

--- OOP with Java/Practical-6/Program2.java
+++ OOP with Java/Practical-6/Program2.java
@@ -45,15 +45,15 @@ class Employee {
 }
 public class Program2 {
     public static void main(String[] args) {
-        Employee emp1 = new Employee(1111, "Jeet", 20);
+        Employee emp1 = new Employee(1111, "Employee 1", 20);
         Employee emp2 = new Employee(emp1); // Using the copy constructor
         System.out.println(emp2.getId()); 
         System.out.println(emp2.getName()); 
         System.out.println(emp2.getAge()); 
 
         emp2.setId(2222);
-        emp2.setName("Rishi");
-        emp2.setAge(40);
+        emp2.setName("Employee 2");
+        emp2.setAge(23);
         
         System.out.println(emp1.getId()); 
         System.out.println(emp1.getName());
